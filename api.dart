204a81c27f40eb56c866a30cdd5222cc9f684d8c
34sea <domingosmartinho67@gmail.com>
2025-04-10 14:48:44 +0200

    others api

--- api.dart
+++ api.dart
@@ -1,6 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+class Album {
+  var id;
+  var userId;
+  var title;
+
+  Album(this.id, this.userId, this.title);
+
+
+}
+
 Future<void> fetchData() async {
   var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
   var response = await http.get(url);
@@ -17,12 +27,66 @@ Future<void> fetchData() async {
 }
 
 
-Future<void> readData(){
-  
+Future<void> readData()async{
+  try{
+    var BASE_URL = Uri.parse("https://jsonplaceholder.typicode.com/comments");
+    var response = await http.get(BASE_URL);
+    if(response.statusCode == 200){
+      List<dynamic> data = jsonDecode( response.body) as List<dynamic>;
+
+      for(final dt in data){
+        print(dt['name']);
+        print("--------------------------------");
+      }
+    }
+  }catch(e){
+    print(e);
+  }
+}
+
+
+Future<void> readDataAlbum()async{
+  try{
+
+    var BASE_URL = Uri.parse("https://jsonplaceholder.typicode.com/albums");
+    var response = await http.get(BASE_URL);
+    if(response.statusCode == 200){
+      List<dynamic> data = jsonDecode(response.body) as List<dynamic>;
+      
+      for(final items in data){
+       print(items['id']);
+      }
+    }
+
+  }catch(e){
+    print(e);
+  }
+}
+
+Future<void> readDataPhotos()async{
+  try{
+    var BASE_URL = Uri.parse("https://jsonplaceholder.typicode.com/photos");
+    var response = await http.get(BASE_URL);
+    if(response.statusCode == 200){
+      List<dynamic> data = jsonDecode(response.body) as List<dynamic>;
+
+      for(final items in data){
+        print(items['url']);
+      }
+    }
+  }catch(e){
+    print(e);
+  }
 }
 
 
 
+
+
 void main() {
-  fetchData();
+  // fetchData();
+  // readData();
+  // readDataAlbum();
+  readDataPhotos();
+  
 }
